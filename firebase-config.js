44cc6ed2e922a11fe60e6f59f70c030ad8ab1cd4
harjeetsingh8802-star <harjeet.singh8802@gmail.com>
2026-09-04 10:27:53 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDeeMQploVOBrMyaVWQLUIgwQyVQ60ZfB0",
+  authDomain: "saratec-quotation.firebaseapp.com",
+  projectId: "saratec-quotation",
+  storageBucket: "saratec-quotation.firebasestorage.app",
+  messagingSenderId: "856410846697",
+  appId: "1:856410846697:web:aa491f55abe772e006c778",
+  measurementId: "G-KCK7D501KT"
+};
